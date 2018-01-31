c52204e14b9aeddb521b238f275a00023ac6ca0b
loacl <loacl>
2018-01-31 17:08:20 +0800

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -0,0 +1,65 @@
+package com.example.xyzreader.remote;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
+
+import java.io.IOException;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RemoteEndpointUtil {
+    private static final String TAG = "RemoteEndpointUtil";
+
+    private RemoteEndpointUtil() {
+    }
+
+    public static JSONArray fetchJsonArray() {
+        String itemsJson = null;
+        try {
+            itemsJson = fetchPlainText(Config.BASE_URL);
+        } catch (IOException e) {
+            Log.e(TAG, "Error fetching items JSON", e);
+            return null;
+        }
+
+        // Parse JSON
+        try {
+            JSONTokener tokener = new JSONTokener(itemsJson);
+            Object val = tokener.nextValue();
+            if (!(val instanceof JSONArray)) {
+                throw new JSONException("Expected JSONArray");
+            }
+            return (JSONArray) val;
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing items JSON", e);
+        }
+
+        return null;
+    }
+
+    static String fetchPlainText(URL url) throws IOException {
+        /**
+        ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.COMPATIBLE_TLS)
+                .tlsVersions(TlsVersion.TLS_1_2)
+                .cipherSuites(
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+                .build();
+         **/
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
